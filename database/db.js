9f965154f5a54c565f6e6668b61510633730ddba
Cmoy07 <cmoyer07@comcast.net>
2026-03-31 11:46:57 -0400

    fixed db storage bug

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const {Sequelize}= require("sequelize");
 // Init database connection
 const db = new Sequelize({
     dialect: 'sqlite',
-    storage: `database/${process.env.DB_NAME}` || 'planner.db',
+    storage: `database/${process.env.DB_NAME || "planner.db"}`,
     logging: false
 });
 
